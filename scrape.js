13d0183481af81a0843920f4548b5024cb2180c5
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2026-05-26 12:19:49 -0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -5,20 +5,39 @@ const puppeteer = require('puppeteer');
 
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
     });
 
     const page = await browser.newPage();
 
+    // Simular navegador real
+    await page.setUserAgent(
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    );
+
+    await page.setViewport({
+        width: 1366,
+        height: 768
+    });
+
     await page.goto(
         'https://circulorafaela.com.ar/farmacias',
         {
-            waitUntil: 'networkidle2',
+            waitUntil: 'domcontentloaded',
             timeout: 0
         }
     );
 
-    await page.waitForSelector('.widget-farmacia');
+    // esperar un poco
+    await new Promise(r => setTimeout(r, 5000));
+
+    // DEBUG
+    const html = await page.content();
+
+    fs.writeFileSync('debug.html', html);
 
     const farmacias = await page.evaluate(() => {
 
